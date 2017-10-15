57017953b050ecf787c008ae0c4f5aad09991407
Shane MacPhillamy <rcoaic@gmail.com>
2017-10-15 15:06:50 +1100

    Initial commit

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 Create Big Data Playground
 
 # Dependencies
-Java 9
+- Java 9
+- kafka_2.12-0.11.0.1
+- Scala 2.12
---
+++ scripts/initial-setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Kafka
+#
+wget http://mirror.ventraip.net.au/apache/kafka/0.11.0.1/kafka_2.12-0.11.0.1.tgz
+#
+# Scala Kafka Streams Examples
+#
+git clone git@github.com:coaic/kafka-streams-scala-examples.git
+#
+# JSON data generator
+#
+git clone git@github.com:coaic/json-data-generator.git
